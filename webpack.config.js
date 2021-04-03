5547aa51a5319a66b7a8f9fe371a0fe00c54c92a
Liew Xun <damakuno@gmail.com>
2021-04-03 22:51:25 +0800

    updated packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-    entry: './public/app.js',
-    output: {
-      path: __dirname + '/public/dist',
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [{
-        test: /\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader'
-        ]
-      }]
-    }
+  entry: './public/app.js',
+  output: {
+    path: __dirname + '/public/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      loader: 'css-loader'
+    }]
   }
+}
