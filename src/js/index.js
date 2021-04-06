c9dc0be47615190fc25ab6fc1af672c5ab7fb4f0
Liew Xun <damakuno@gmail.com>
2021-04-06 22:27:55 +0800

    added animation termination on pause

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,17 @@ const bpm_to_interval = (value) => {
     return (1 / bps) * 1000;
 };
 
-let dataArray = new Uint8Array(40);
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+const arrSize = 40;
+
+let dataArray = new Uint8Array(arrSize);
+
+let pauseFlag = false;
+let animeTimer;
+
 
 window.onload = () => {
     let beat_animation = anime({
@@ -15,14 +25,14 @@ window.onload = () => {
         duration: bpm_to_interval(92),
         autoplay: false
     });
-    beat_animation.play();
     console.log('index.js loaded.');
 
     let audio_player = document.querySelector('#audio_player');
-    audio_player.volume = 0.4;
+    audio_player.volume = 0.5;
     audio_player.play();
 
     audio_player.addEventListener('play', (e) => {
+        pauseFlag = false;
         console.log('media playing');
         setTimeout(() => {
             let stream = audio_player.captureStream();
@@ -39,27 +49,39 @@ window.onload = () => {
             scriptProcessor.connect(audioContext.destination);
 
             beat_animation.play();
-            setInterval(() => {
+            animeTimer = setInterval(() => {
                 // let dataArray = new Uint8Array(analyzer.frequencyBinCount);
+                let prevArray = [...dataArray];
                 analyzer.getByteFrequencyData(dataArray);
                 dataArray.forEach((value, index) => {
+                    let dy = Math.abs(value - prevArray[index]);
                     anime({
                         targets: `.item-visualizer div:nth-child(${index + 1})`,
+                        translateY: dy > 10 ? `-${value * 2.5}px` : `-${prevArray[index] * 2.5}px`,
                         translateY: `-${value * 2.5}px`,
-                        duration: bpm_to_interval(92 * 1.5),
-                        easing: 'easeInOutCirc',
-                        direction: 'alternate'
+                        duration: bpm_to_interval(92 * (4 - scale(dy, 0, 255, 0, 3))),
+                        easing: 'linear',
+                        complete: (anim) => {
+                            if (pauseFlag) {
+                                anime({
+                                    targets: `.item-visualizer div:nth-child(${index + 1})`,
+                                    translateY: `0px`,
+                                    duration: bpm_to_interval(92),
+                                    easing: 'easeInOutCirc'
+                                });
+                            }
+                        }
                     });
                 });
-            }, bpm_to_interval(92));
+            }, bpm_to_interval(92 * 4));
 
-
-
-        }, 100);
+        }, 500);
     });
 
     audio_player.addEventListener('pause', (e) => {
+        pauseFlag = true;
         console.log('media paused');
         beat_animation.pause();
+        clearInterval(animeTimer);
     });
 };
